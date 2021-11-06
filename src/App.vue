b07f784d886fb246c6f30049bfd8b5ef1d21a7a9
fmarcelomorais <marcelomorais.program@gmail.com>
2021-11-06 20:41:04 -0300

    Commit do projeto

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+		<TaskProgress :progress="progress"/>
+		<NewTask @taskAdded="addTask"/>
+		<TaskGrid 
+			@taskDeleted="taskDeleted"
+			@taskStateChange="toggleTaskState"
+			:tasks="tasks" />
+	</div>
+</template>
+
+<script>
+import NewTask from './componentes/NewTask.vue'
+import TaskGrid from './componentes/TaskGrid.vue'
+import TaskProgress from './componentes/TaskProgress.vue'
+
+
+export default {
+	components: {
+		TaskGrid,
+		NewTask,
+		TaskProgress
+	},
+	data(){
+		return{
+			tasks:[]
+		}
+	},
+	
+	computed:{
+		progress(){
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
+
+		}
+	},
+	watch:{
+		tasks:{
+			deep: true,
+			handler(){
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
+	methods:{
+		addTask(task){
+			if(task.name != ''){
+				const sameName = t => t.name === task.name
+				const reallyNew = this.tasks.filter(sameName).length == 0
+				if(reallyNew){			
+					this.tasks.push({
+						name: task.name,
+						pending: task.pending || true
+					})
+				}
+			}
+		},
+		taskDeleted(id){
+			this.tasks.splice(id, 1)
+		},
+		toggleTaskState(id){
+			this.tasks[id].pending = !this.tasks[id].pending
+		}
+	},
+	created(){
+		const json = localStorage.getItem('tasks')
+		const array = JSON.parse(json)
+		this.tasks = Array.isArray(array) ?  array  : []
+	}
+	
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Lato', sans-serif;
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+
+	
+</style>
---
+++ src/componentes/NewTask.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="new-task">
+      <input type="text"
+        v-model="name"
+        @keydown.enter="add"
+        class="form-element"
+        placeholder="Nova Tarefa"
+      >
+      <button @click="add" class="form-element">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name: ''
+        }
+    },
+    methods:{
+        add(){
+            this.$emit('taskAdded', { name: this.name })
+            this.name = ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .new-task{
+        margin: 35px;
+    }
+    .form-element{
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid #FFF;
+        padding: 5px 10px 8px;
+        color: #fff;
+        
+    }
+    input.form-element{
+        width: 500px;
+        background: #fff2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+    button.form-element{
+        border-left: none;
+        background: rgba(10, 238, 21, 0.473);
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+        cursor: pointer;
+    }
+    
+</style>
